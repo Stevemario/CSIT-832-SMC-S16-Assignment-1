162e7f5e6cbf940b8e5d3f10e9cb55074c20dce2
stevemario <steve.mario.correa@mail.com>
2016-02-08 22:50:47 -0800

    made spaces conform

--- pqtype.cpp
+++ pqtype.cpp
@@ -1,35 +1,35 @@
 template<class ItemType>
 PQType<ItemType>::PQType (int max) {
-  maxItems = max;
-  items.elements = new ItemType[max];
-  length = 0;
+	maxItems = max;
+	items.elements = new ItemType[max];
+	length = 0;
 }
 template<class ItemType>
 PQType<ItemType>::~PQType () {
-  delete [] items.elements;
+	delete [] items.elements;
 }
 template<class ItemType>
 void PQType<ItemType>::MakeEmpty () {
-  length = 0;
+	length = 0;
 }
 template<class ItemType>
 bool PQType<ItemType>::IsEmpty () {
-  return (length == 0);
+	return (length == 0);
 }
 template<class ItemType>
 bool PQType<ItemType>::IsFull () {
-  return (length == maxItems);
+	return (length == maxItems);
 }
 template<class ItemType>
 void PQType<ItemType>::Enqueue (ItemType newItem) {
-     length++;
-    items.elements[length-1] = newItem;
-    items.ReheapUp (0, length-1);
+	length++;
+	items.elements[length-1] = newItem;
+	items.ReheapUp (0, length-1);
 }
 template<class ItemType>
 void PQType<ItemType>::Dequeue (ItemType& item) {
-     item = items.elements[0];
-    items.elements[0] = items.elements[length-1];
-    length--;
-    items.ReheapDown (0, length-1);
+	item = items.elements[0];
+	items.elements[0] = items.elements[length-1];
+	length--;
+	items.ReheapDown (0, length-1);
 }
