1ee2dcb37eef29d6b05d8d40decd29e73d547568
stevemario <steve.mario.correa@mail.com>
2016-02-17 22:37:04 -0800

    removed comment

--- heap.h
+++ heap.h
@@ -1,7 +1,5 @@
 #ifndef HEAP_H
 #define HEAP_H
-// Assumes ItemType either a built-in simple data
-// type or a class with overloaded relational operators.
 template<class HeapType>
 struct Heap {
 	void ReheapDown (int, int);
