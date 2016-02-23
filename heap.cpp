494c0364e98da1091a24173524e60764791362a6
stevemario <steve.mario.correa@mail.com>
2016-02-23 11:18:33 -0800

    renamed index variables in heap functions to make it clear that they are indexes

--- heap.cpp
+++ heap.cpp
@@ -1,14 +1,14 @@
 //  IMPLEMENTATION  OF RECURSIVE HEAP MEMBER FUNCTIONS
 template<class HeapType>
-void Heap<HeapType>::ReheapUp (int nNodeIndex, int nGenerations, const int nElements)
+void Heap<HeapType>::ReheapUp (int nIndexOfNodeToMove, int nGenerations, const int nElements)
 //This function checks
 //if a node has greater priority than a number of its predecessors, and
 //if it does,
 //it swaps the node's place in the heap with its predecessors.
 {
-	int nParentNodeIndex;
+	int nIndexOfNodeParent;
 
-	if (nNodeIndex > 0 && nNodeIndex < nElements && nGenerations > 0) {
+	if (nIndexOfNodeToMove > 0 && nIndexOfNodeToMove < nElements && nGenerations > 0) {
 	//if the node's index is positive, and
 	//   the node's index is an index for a placed node, and
 	//   the number of generations to heap up is positive
@@ -26,11 +26,11 @@ void Heap<HeapType>::ReheapUp (int nNodeIndex, int nGenerations, const int nElem
 		//Right children's indexes are always even, meaning
 		//right children's indexes % 2 will equal 0.
 
-		if (nNodeIndex % 2 == 0) {
+		if (nIndexOfNodeToMove % 2 == 0) {
 		//if the node's index is even, meaning
 		//if the node is a right child
 
-			nParentNodeIndex = (nNodeIndex - 2) / 2;
+			nIndexOfNodeParent = (nIndexOfNodeToMove - 2) / 2;
 			//the node's parent's index is the difference of
 			//the node's index and two,
 			//divided by two.
@@ -39,7 +39,7 @@ void Heap<HeapType>::ReheapUp (int nNodeIndex, int nGenerations, const int nElem
 		//if the node's index is odd, meaning
 		//if the node is a left child
 
-			nParentNodeIndex = (nNodeIndex - 1) / 2;
+			nIndexOfNodeParent = (nIndexOfNodeToMove - 1) / 2;
 			//the node's parent's index is the difference of
 			//the node's index and one,
 			//divided by two.
@@ -50,7 +50,7 @@ void Heap<HeapType>::ReheapUp (int nNodeIndex, int nGenerations, const int nElem
 		//PROFESSOR KENT'S CODE:
 		//parent = (bottom - 1) / 2;
 
-		if (elements[nParentNodeIndex] < elements[nNodeIndex]) {
+		if (elements[nIndexOfNodeParent] < elements[nIndexOfNodeToMove]) {
 		//if the parent node's priority
 		//is lesser than
 		//the child node's priority
@@ -58,72 +58,72 @@ void Heap<HeapType>::ReheapUp (int nNodeIndex, int nGenerations, const int nElem
 			//the heap's rules are violated.
 			//To fix that we start by
 
-			Swap (elements[nParentNodeIndex], elements[nNodeIndex]);
+			Swap (elements[nIndexOfNodeParent], elements[nIndexOfNodeToMove]);
 			//swapping the node with it's parent
 
 			nGenerations--;
 			//note that we've gone up a generation
 
-			ReheapUp (nParentNodeIndex, nGenerations, nElements);
+			ReheapUp (nIndexOfNodeParent, nGenerations, nElements);
 			//and repeating until the requirements are met.
 		}
 	}
 }
 template<class HeapType>
-void Heap<HeapType>::ReheapDown (int nNodeIndex, int nGenerations, const int nElements)
+void Heap<HeapType>::ReheapDown (int nIndexOfNodeToMove, int nGenerations, const int nElements)
 //This function checks
 //if a node has lesser priority than a number of its children, and
 //if it does,
 //it swaps the node's place in the heap with its successor.
 {
-	int maxChild;
-	int rightChild;
-	int leftChild;
+	int nIndexOfNodeChildLeft;
+	int nIndexOfNodeChildRight;
+	int nIndexOfNodeChildWithHigherPriority;
 
-	leftChild = 2 * nNodeIndex + 1;
-	rightChild = 2 * nNodeIndex + 2;
+	nIndexOfNodeChildLeft = 2 * nIndexOfNodeToMove + 1;
+	nIndexOfNodeChildRight = 2 * nIndexOfNodeToMove + 2;
 
-	if (nNodeIndex >= 0 && nNodeIndex < nElements  && nGenerations > 0) {
+	if (nIndexOfNodeToMove >= 0 && nIndexOfNodeToMove < nElements  && nGenerations > 0) {
 	//if the node's index is positive, or zero, and
 	//   the node's index is an index for a placed node, and
 	//   the number of generations to heap up is positive.
 
-		if (leftChild <= nElements - 1) {
+		if (nIndexOfNodeChildLeft <= nElements - 1) {
 		//if the left child is within the heap
 
-			if (leftChild == nElements - 1)
+			if (nIndexOfNodeChildLeft == nElements - 1)
 			//if the left child is the last element
 
-				maxChild = leftChild;
+				nIndexOfNodeChildWithHigherPriority = nIndexOfNodeChildLeft;
 				//the right child is not within the heap, so
-				//the left child is the bigger valid child
+				//the left child is the higher priority valid child
 
 			else {
 			//if the left child is not the last element, meaning
 			//both left and right children could be promoted
 
-				if (elements[leftChild] < elements[rightChild])
+				if (elements[nIndexOfNodeChildLeft] < elements[nIndexOfNodeChildRight])
 				//if the left child's priority
 				//is lesser than
 				//the right child's priority
 
-					maxChild = rightChild;
-					//the right child is the bigger child
+					nIndexOfNodeChildWithHigherPriority = nIndexOfNodeChildRight;
+					//the right child is the higher priority child
 
 				else
 				//if the left child's priority
 				//is not lesser than
 				//the right child's priority
 
-					maxChild = leftChild;
-					//the left child is the bigger child,
+					nIndexOfNodeChildWithHigherPriority = nIndexOfNodeChildLeft;
+					//the left child is the higher priority child,
 					//in this case, due the job class member
 					//job will prevent equal priority.
 					//in other cases, ties are possible,
 					//but not important, left child is chosen
 					//just because one has to be chosen.
 			}
-			if (elements[nNodeIndex] < elements[maxChild]) {
+			if (elements[nIndexOfNodeToMove] < elements[nIndexOfNodeChildWithHigherPriority]) {
 			//if the node's priority
 			//is lesser than
 			//the bigger child's priority
@@ -131,13 +131,13 @@ void Heap<HeapType>::ReheapDown (int nNodeIndex, int nGenerations, const int nEl
 				//the heap's rules are violated.
 				//To fix that we start by
 
-				Swap (elements[nNodeIndex], elements[maxChild]);
+				Swap (elements[nIndexOfNodeToMove], elements[nIndexOfNodeChildWithHigherPriority]);
 				//swapping the node with it's bigger child
 
 				nGenerations--;
 				//note that we've gone down a generation
 
-				ReheapDown (maxChild, nGenerations, nElements);
+				ReheapDown (nIndexOfNodeChildWithHigherPriority, nGenerations, nElements);
 				//and repeating until the requirements are met.
 			}
 		}
